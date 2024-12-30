642b49d863cb53461eaaceaa2086cde9da9eada0
Gnanashekar045 <gnanashekar045@gmail.com>
2024-12-30 21:44:39 +0530

    Update users.jsx

--- apireact/src/component/users.jsx
+++ apireact/src/component/users.jsx
@@ -13,7 +13,7 @@ const Users = () => {
   useEffect(() => {
     readUser();
   }, []);
-//handle change for
+//handle change for in 
   const handleChange = (e) => {
     const { name, value, type, checked, files } = e.target;
 
